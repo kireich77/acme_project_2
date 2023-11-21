0cf1727822f0af8424c6ded188b94886f256c8e2
kireich77 <kireich@gmail.com>
2023-11-21 16:47:11 +0300

    работа с формафи

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,10 +1,13 @@
 # birthday/models.py
 from django.db import models
-
+# Импортируется функция-валидатор.
+from .validators import real_age
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
         'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
     )
-    birthday = models.DateField('Дата рождения')
+     # Валидатор указывается в описании поля.
+    birthday = models.DateField('Дата рождения', validators=(real_age,)) 
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
